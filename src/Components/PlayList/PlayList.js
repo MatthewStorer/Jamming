9f9e212b503d23a29721df03ec2da3f127d2c681
matthew <matthewstorer@outlook.com.au>
2017-09-19 13:53:36 +1000

    revision 9

--- src/Components/PlayList/PlayList.js
+++ src/Components/PlayList/PlayList.js
@@ -24,7 +24,7 @@ class PlayList extends React.Component
 {
   return (<div className="Playlist">
     <input value="New Playlist" onChange={this.handleNameChange} />
-    <TrackList Tracks={this.props.playListTracks} onRemove={this.props.onRemove} />
+    <TrackList tracks={this.props.playListTracks} onRemove={this.props.onRemove} />
     <a className="Playlist-save" onClick={this.props.onSave}>SAVE TO SPOTIFY</a>
   </div>
 
